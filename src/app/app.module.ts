e5035112152deadbda5d0d7fde3a5eecb30d72cc
sayeed1999 <mdsayeedrahman1999@gmail.com>
2021-08-17 15:51:29 +0600

    angular material added, a custom button for the home created in the shared folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { HomeButtonComponent } from './shared/home-button/home-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeButtonComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+
+const MaterialComponents : any[] = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule,
+  MatTableModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule,
+  MatSnackBarModule,
+  MatAutocompleteModule,
+  MatDatepickerModule,
+];
+
+@NgModule({
+  imports: [...MaterialComponents],
+  exports: [...MaterialComponents],
+})
+export class MaterialModule { }
