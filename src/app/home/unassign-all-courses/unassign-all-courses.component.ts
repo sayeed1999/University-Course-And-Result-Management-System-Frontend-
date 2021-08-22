4587102a567c75d16b3758b10862d52f482be748
sayeed1999 <mdsayeedrahman1999@gmail.com>
2021-08-22 13:55:46 +0600

    feature13: unassign courses completed

--- src/app/home/unassign-all-courses/unassign-all-courses.component.ts
+++ src/app/home/unassign-all-courses/unassign-all-courses.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CoursesService } from 'src/app/services/courses.service';
 
 @Component({
   selector: 'app-unassign-all-courses',
@@ -9,17 +11,31 @@ export class UnassignAllCoursesComponent implements OnInit {
 
   title = 'Unassign All Courses';
 
-  constructor() { }
+  constructor(
+    private courseService: CoursesService,
+    private snackbar: MatSnackBar,
+  ) { }
 
   ngOnInit(): void {
   }
 
   clicked() {
     if(confirm("Are you sure to unassign all courses?")) {
-
+      this.unassign();
     } else {
-      
+
     }
+  }
+
+  unassign() {
+    this.courseService.UnassignAll().subscribe(
+      res => {
+        this.snackbar.open(res.message, 'Close');
+      },
+      error => {
+        this.snackbar.open(error.error.message ?? "Some error occurred", 'Close');
+      }
+    );
   }
 
 }
