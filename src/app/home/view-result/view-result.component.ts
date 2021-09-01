ae67aae81d56cd80405293ce2a0148d6197260a7
sayeed1999 <mdsayeedrahman1999@gmail.com>
2021-09-01 13:46:27 +0600

    removed unnecessary export to pdf code

--- src/app/home/view-result/view-result.component.ts
+++ src/app/home/view-result/view-result.component.ts
@@ -55,20 +55,4 @@ export class ViewResultComponent implements OnInit {
     this.router.navigate([this.router.url, 'result-sheet-generation'], { state: this.student.value });
   }
 
-  exportHtmlToPDF() {
-    // make pdf
-    let data = document.getElementById('body') ?? document.createElement('div');
-    html2canvas(data).then(canvas => {
-    
-      let fileWidth = 208;
-      let fileHeight = canvas.height * fileWidth / canvas.width;
-      
-      const FILEURI = canvas.toDataURL('image/png')
-      let PDF = new jsPDF('p', 'mm', 'a4');
-      let position = 0;
-      PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight)
-      
-      PDF.save('angular-demo.pdf');
-    });     
-  }
 }
